44067b11f922ceb3c7ee90fb49ce430b0e50a631
Pascal <pm98zz.c@gmail.com>
2022-04-20 17:51:50 +0200

    Move from gist to own repo

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # in-outdoor
-A tiny shell script to switch the Gnome desktop environment from dark/to light theme
+A tiny shell script to switch the Gnome desktop environment from dark/to light theme. I'm using for when using my laptop from indoor to outdoor, but can be used for night/day times too.
+
+It supports both standard GTK3 themes and the newer Gnome 42 global "dark mode", ensuring all application are toggled regardless of which one they use.
+
+Additionally, it can also toggle:
+- VS Code themes
+- Screen brightness
+
+## Usage
+Simply call ./in-outdoor.sh to toggle between the two configurations. You can of course add an alias in your .bashrc/.zshrc (eg `alias in-out="/bin/sh /home/me/path/to/in-out/in-outdoor.sh"
+`) or assign it a keyboard shortcut.
+
+## Dependencies
+The only hard dependency is Gnome's gsettings utility. To be able to set the screen brightness, you'll need https://github.com/Hummer12007/brightnessctl (available in most distros).
+
+## Configuration
+You can customize a few things by creating a ~/.config/in-out.ini file.
+The default values for the available options are:
+```
+# file ~/.config/in-out.ini
+
+### Gnome 3+/Legacy GTK
+# To see which themes are available, use gnome-tweak-tool. You can get the currently active theme with `gsettings get org.gnome.desktop.interface gtk-theme`.
+GTK_IN_THEME="Adwaita-dark"
+GTK_OUT_THEME="HighContrast"
+
+### Gnome 42+
+SHELL_IN_THEME="prefer-dark"
+SHELL_OUT_THEME="prefer-light"
+
+### VSCode @see https://code.visualstudio.com/
+VS_CODE_IN_THEME="Default Dark+"
+VS_CODE_OUT_THEME="Default Light+"
+
+### Screen brightness @see https://github.com/Hummer12007/brightnessctl
+BRIGHTNESS_IN="85%"
+BRIGHTNESS_OUT="100%"
+
+```
+
+## Known issues / limitations
+
+This was created for my personal use, and tested only on Debian, it might not fit your use case or work on your setup.
---
+++ in-outdoor.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+### Switch between outdoor and indoor settings.
+
+### Gnome 3+/Legacy GTK
+GTK_IN_THEME="Adwaita-dark"
+GTK_OUT_THEME="Adwaita"
+### Gnome 42+
+SHELL_IN_THEME="prefer-dark"
+SHELL_OUT_THEME="prefer-light"
+### VSCode @see https://code.visualstudio.com/
+VS_CODE_IN_THEME="Default Dark+"
+VS_CODE_OUT_THEME="Default Light+"
+### Screen brightness @see https://github.com/Hummer12007/brightnessctl
+BRIGHTNESS_IN="85%"
+BRIGHTNESS_OUT="100%"
+
+### Load configuration.
+if [ -z "$XDG_CONFIG_HOME" ]; then
+  XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+if [ -f "$XDG_CONFIG_HOME/in-out.ini" ]; then
+  # shellcheck source=/dev/null
+  . "$XDG_CONFIG_HOME/in-out.ini"
+fi
+
+# Default to "in" theme.
+GTK_SWITCH_THEME="$GTK_IN_THEME"
+SHELL_SWITCH_THEME="$SHELL_IN_THEME"
+BRIGHTNESS_SWITCH="$BRIGHTNESS_IN"
+SWITCH_IS="in"
+
+switch_out(){
+  GTK_SWITCH_THEME="$GTK_OUT_THEME"
+  SHELL_SWITCH_THEME="$SHELL_OUT_THEME"
+  BRIGHTNESS_SWITCH="$BRIGHTNESS_OUT"
+  SWITCH_IS="out"
+}
+
+# First try to use the newer dark mode.
+CURRENT_THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
+COMPARE_THEME="$SHELL_IN_THEME"
+# Not yet on gnome 42, fallback to legacy theme.
+if [ -z "$CURRENT_THEME" ]; then
+  CURRENT_THEME="$(gsettings get org.gnome.desktop.interface gtk-theme)"
+  COMPARE_THEME="$GTK_IN_THEME"
+fi
+# See if we need to trigger switch. Themes are stored within single quotes.
+CURRENT_THEME=$(echo "$CURRENT_THEME" | sed "s/'//g")
+if [ "$CURRENT_THEME" = "$COMPARE_THEME" ]; then
+  switch_out
+fi
+
+################################# Actual processing.
+
+### Gnome-shell
+gsettings set org.gnome.desktop.interface gtk-theme "$GTK_SWITCH_THEME"
+gsettings set org.gnome.desktop.wm.preferences theme "$GTK_SWITCH_THEME"
+gsettings set org.gnome.desktop.interface color-scheme "$SHELL_SWITCH_THEME"
+
+### Visual Studio Code.
+VS_CODE_SETTINGS="$XDG_CONFIG_HOME/Code/User/settings.json"
+if [ -f "$VS_CODE_SETTINGS" ]; then
+    REPLACE="s/$VS_CODE_OUT_THEME/$VS_CODE_IN_THEME/g"
+    if [ "$SWITCH_IS" = "out" ]; then
+      REPLACE="s/$VS_CODE_IN_THEME/$VS_CODE_OUT_THEME/g"
+    fi
+  sed -e "$REPLACE" "$VS_CODE_SETTINGS" > /tmp/vscsettings.tmp && mv /tmp/vscsettings.tmp "$VS_CODE_SETTINGS"
+fi
+
+### Screen brightness
+if command -v brightnesscastl ; then
+  brightnessctl set "$BRIGHTNESS_SWITCH" > /dev/null
+fi
