5937d37c90842e888f6f5f52ac069ca51575e605
Pascal <pm98zz.c@gmail.com>
2026-07-27 18:11:47 +0200

    Update default VSCode theme names

--- in-outdoor.sh
+++ in-outdoor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # in-outdoor.sh
-# Copyright (C) 2022  Pascal Morin
+# Copyright (C) 2022 Pascal Morin
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -10,11 +10,11 @@
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see http://www.gnu.org/licenses .
 
 ### Gnome 3+/Legacy GTK
 GTK_IN_THEME="Adwaita-dark"
@@ -26,20 +26,20 @@ SHELL_OUT_THEME="prefer-light"
 NIGHT_LIGHT_IN="true"
 NIGHT_LIGHT_OUT="false"
 ### VSCode @see https://code.visualstudio.com/
-VS_CODE_IN_THEME="Default Dark Modern"
-VS_CODE_OUT_THEME="Default Light Modern"
+VS_CODE_IN_THEME="Dark Modern"
+VS_CODE_OUT_THEME="Light Modern"
 ### Screen brightness @see https://github.com/Hummer12007/brightnessctl
 BRIGHTNESS_IN="85%"
 BRIGHTNESS_OUT="100%"
 
 ### Load configuration.
 if [ -z "$XDG_CONFIG_HOME" ]; then
-  XDG_CONFIG_HOME="$HOME/.config"
+XDG_CONFIG_HOME="$HOME/.config"
 fi
 
 if [ -f "$XDG_CONFIG_HOME/in-out.ini" ]; then
-  # shellcheck source=/dev/null
-  . "$XDG_CONFIG_HOME/in-out.ini"
+# shellcheck source=/dev/null
+. "$XDG_CONFIG_HOME/in-out.ini"
 fi
 
 # Default to "in" theme.
@@ -50,11 +50,11 @@ SWITCH_IS="in"
 NIGHT_LIGHT_SWITCH="$NIGHT_LIGHT_IN"
 
 switch_out(){
-  GTK_SWITCH_THEME="$GTK_OUT_THEME"
-  SHELL_SWITCH_THEME="$SHELL_OUT_THEME"
-  BRIGHTNESS_SWITCH="$BRIGHTNESS_OUT"
-  NIGHT_LIGHT_SWITCH="$NIGHT_LIGHT_OUT"
-  SWITCH_IS="out"
+GTK_SWITCH_THEME="$GTK_OUT_THEME"
+SHELL_SWITCH_THEME="$SHELL_OUT_THEME"
+BRIGHTNESS_SWITCH="$BRIGHTNESS_OUT"
+NIGHT_LIGHT_SWITCH="$NIGHT_LIGHT_OUT"
+SWITCH_IS="out"
 }
 
 # First try to use the newer dark mode.
@@ -62,13 +62,13 @@ CURRENT_THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
 COMPARE_THEME="$SHELL_IN_THEME"
 # Not yet on gnome 42, fallback to legacy theme.
 if [ -z "$CURRENT_THEME" ]; then
-  CURRENT_THEME="$(gsettings get org.gnome.desktop.interface gtk-theme)"
-  COMPARE_THEME="$GTK_IN_THEME"
+CURRENT_THEME="$(gsettings get org.gnome.desktop.interface gtk-theme)"
+COMPARE_THEME="$GTK_IN_THEME"
 fi
 # See if we need to trigger switch. Themes are stored within single quotes.
 CURRENT_THEME=$(echo "$CURRENT_THEME" | sed "s/'//g")
 if [ "$CURRENT_THEME" = "$COMPARE_THEME" ]; then
-  switch_out
+switch_out
 fi
 
 ################################# Actual processing.
@@ -82,14 +82,14 @@ gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled "$NIGH
 ### Visual Studio Code.
 VS_CODE_SETTINGS="$XDG_CONFIG_HOME/Code/User/settings.json"
 if [ -f "$VS_CODE_SETTINGS" ]; then
-    REPLACE="s/$VS_CODE_OUT_THEME/$VS_CODE_IN_THEME/g"
-    if [ "$SWITCH_IS" = "out" ]; then
-      REPLACE="s/$VS_CODE_IN_THEME/$VS_CODE_OUT_THEME/g"
-    fi
-  sed -e "$REPLACE" "$VS_CODE_SETTINGS" > /tmp/vscsettings.tmp && mv /tmp/vscsettings.tmp "$VS_CODE_SETTINGS"
+REPLACE="s/$VS_CODE_OUT_THEME/$VS_CODE_IN_THEME/g"
+if [ "$SWITCH_IS" = "out" ]; then
+REPLACE="s/$VS_CODE_IN_THEME/$VS_CODE_OUT_THEME/g"
+fi
+sed -e "$REPLACE" "$VS_CODE_SETTINGS" > /tmp/vscsettings.tmp && mv /tmp/vscsettings.tmp "$VS_CODE_SETTINGS"
 fi
 
 ### Screen brightness
 if command -v brightnessctl > /dev/null ; then
-  brightnessctl set "$BRIGHTNESS_SWITCH" > /dev/null
+brightnessctl set "$BRIGHTNESS_SWITCH" > /dev/null
 fi
